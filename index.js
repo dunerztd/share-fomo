91c4d14ff7c309a03cac442b68572dd24195b252
Tom D <dunerz@hotmail.com>
2020-09-21 13:39:57 +1000

    updates

--- index.js
+++ index.js
@@ -2,16 +2,28 @@
 
 // function getFormText() {
 
-// }
+let amount = 'hello'
 
 const form = document.querySelector('.form');
 // const username = document.querySelector('#username');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+
+  
   // console.log('form submitted');
   // console.log(username.value);
-  console.log(typeof(form.amount.value));
-  console.log(typeof(form.ticker.value));
-  console.log(typeof(form.date.value));
+  console.log(amount);
+  amount = form.amount.value
+  // console.log(typeof(form.ticker.value));
+  // console.log(typeof(form.date.value));
+  console.log(amount);
+
+  
+
 });
+
+function getInputData() {
+  amount = form.amount.value
+}
+console.log(amount);
