fa4697b572c6966f5d2e8400cd082f0842a9eb6a
Nic Meg <nicmeg54@gmail.com>
2020-09-23 10:12:21 +1000

    refactor - order of functions

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 let amount = ""
 let ticker = ""
 let date = ""
-let temp = ""
 let total_shares_purchased_main
 let today_adjusted_close_main
 let total_share_value_main
@@ -14,20 +13,22 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  removeFomoValue()
+  removePreviousQuery()
   showWaitingMessage()
   getInputData()
   getData()
 });
 
-function showWaitingMessage () {
-  var x = document.getElementById("waiting");
-  x.classList.remove('d-none')
+function removePreviousQuery(x) {
+  const content = document.querySelector('.fomo_value');
+  content.innerHTML = ``;
+  // also removes profit_loss value
+  document.getElementById("profit_loss").innerHTML = ``
 }
 
-function hideWaitingMessage () {
-var x = document.getElementById("waiting");
-x.classList.add('d-none')
+function showWaitingMessage() {
+  var x = document.getElementById("waiting");
+  x.classList.remove('d-none')
 }
 
 function getInputData() {
@@ -36,16 +37,77 @@ function getInputData() {
   date = form.date.value
 }
 
-function displayFomoValue(x){
-  const content = document.querySelector('.fomo_value');
-  content.innerHTML = `<h2>Your shares are now worth $${x} USD</h2>`;
+function hideWaitingMessage() {
+  var x = document.getElementById("waiting");
+  x.classList.add('d-none')
 }
 
-function removeFomoValue(x){
+function adjustDateAccordingToDayOfWeek(date) {
+
+  switch (date.getDay()) {
+    case 0:
+      return date.setDate(date.getDate() - 2);
+    case 1:
+      return date.setDate(date.getDate() - 3);
+    default:
+      return date.setDate(date.getDate() - 1);
+  }
+}
+
+function getData() {
+  fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&outputsize=full&apikey=NT8P50P5AD25XXZW`)
+    .then(response => response.json())
+    .then(wrapperFunction)
+    .then(todaysShareValue)
+    .then(displayFomoValue)
+    .then(displayProfitOrLoss)
+}
+
+function wrapperFunction(response) {
+  sharesPurchased(response)
+  mostRecentSharePrice(response) 
+}
+
+
+function sharesPurchased(x) {
+  let amount_to_num = parseFloat(amount)
+  let adjusted_close_to_num = parseFloat(x['Time Series (Daily)'][getMostRecentMarketCloseDate(date)]['5. adjusted close'])
+  let total_shares_purchased = amount_to_num / adjusted_close_to_num
+  total_shares_purchased_main = total_shares_purchased
+}
+
+function mostRecentSharePrice(x) {
+  let today_adjusted_close = parseFloat(x['Time Series (Daily)'][getMostRecentMarketCloseDate()]['5. adjusted close'])
+  today_adjusted_close_main = today_adjusted_close
+}
+
+function todaysShareValue() {
+  total_share_value_main = total_shares_purchased_main * today_adjusted_close_main
+  hideWaitingMessage()
+  return parseFloat(total_share_value_main.toFixed(2))
+}
+
+function getMostRecentMarketCloseDate(buy_date) {
+
+  if (typeof buy_date === 'undefined' || buy_date === null) {
+    d = new Date()
+  } else {
+    d = new Date(buy_date)
+  }
+
+  adjustDateAccordingToDayOfWeek(d)
+
+  let dd = String(d.getDate()).padStart(2, '0');
+  let mm = String(d.getMonth() + 1).padStart(2, '0');
+  let yyyy = d.getFullYear();
+
+  let result = yyyy + '-' + mm + '-' + dd
+  return result
+}
+
+function displayFomoValue(x) {
   const content = document.querySelector('.fomo_value');
-  content.innerHTML = ``;
-  // also removes profit_loss value
-  document.getElementById("profit_loss").innerHTML = ``
+  content.innerHTML = `<h2>Your shares are now worth $${x} USD</h2>`;
 }
 
 function displayProfitOrLoss() {
@@ -64,68 +126,4 @@ function displayProfitOrLoss() {
           <img src="loss.webp" alt="" class="src">
         </div>`
   }
-}
-
-function sharesPurchased(x) {
-  let amount_to_num = parseFloat(amount)
-  let adjusted_close_to_num = parseFloat(x['Time Series (Daily)'][getMostRecentMarketCloseDate(date)]['5. adjusted close'])
-  let total_shares_purchased = amount_to_num/adjusted_close_to_num
-  total_shares_purchased_main = total_shares_purchased
-}
-
-function getMostRecentMarketCloseDate(buy_date) {
- 
-  if (typeof buy_date === 'undefined' || buy_date === null) {
-    d = new Date()
-  } else {
-    d = new Date(buy_date)
-  }
-  
-  adjustDateAccordingToDayOfWeek(d)
-
-  let dd = String(d.getDate()).padStart(2, '0');
-  let mm = String(d.getMonth() + 1).padStart(2, '0');
-  let yyyy = d.getFullYear();
-
-  let result = yyyy + '-' + mm + '-' + dd
-  return result
-}
-
-function adjustDateAccordingToDayOfWeek(date) {
-
-  switch(date.getDay()) {
-    case 0:
-      return date.setDate(date.getDate() - 2);
-    case 1:
-      return date.setDate(date.getDate() - 3);
-    default:
-      return date.setDate(date.getDate() - 1);
-  }
-}
-
-function mostRecentSharePrice(x) {
-
-  fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&outputsize=full&apikey=NT8P50P5AD25XXZW`)
-    .then(response => response.json())
-    .then((data) => {
-      let today_adjusted_close = parseFloat(data['Time Series (Daily)'][getMostRecentMarketCloseDate()]['5. adjusted close'])
-      today_adjusted_close_main = today_adjusted_close
-
-    })
-    .then(todaysShareValue)
-    .then(displayFomoValue)
-    .then(displayProfitOrLoss)
-}
-
-function todaysShareValue() {
-  total_share_value_main = total_shares_purchased_main * today_adjusted_close_main
-  hideWaitingMessage()
-  return parseFloat(total_share_value_main.toFixed(2))
-}
-
-function getData() {
-  fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&outputsize=full&apikey=NT8P50P5AD25XXZW`)
-    .then(response => response.json())
-    .then(sharesPurchased)
-    .then(mostRecentSharePrice)
 }
